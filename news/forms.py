499aa3a464e8b0b17dfb99d9139a7e66d26a772b
root <root@DESKTOP-HLMRNO7.localdomain>
2022-09-07 10:33:54 +0900

    main

---
+++ news/forms.py
@@ -0,0 +1,15 @@
+from cProfile import label
+from django import forms
+from .models import Book
+
+class BookForm(forms.ModelForm):
+      class Meta:
+            model = Book
+            fields = ('title', 'category')
+            labels = {
+                'title': '対象書籍 :',
+                'category': 'カテゴリ :'
+
+            }
+
+
---
+++ news/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from news.consts import MAX_RATE
+# Create your models here.
+
+
+
+
+CATEGORY = (('business', 'ビジネス'), ('science ・Technology', '科学・テクノロジー'), ('Humanities ・ ideas', '人文・思想'), ('computer・IT', 'コンピュータ・IT'))
+RATE_CHOICES = [(x, str(x))for x in range(0, MAX_RATE + 1)]
+
+class Book(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    category = models.CharField(
+        max_length = 100,
+        choices = CATEGORY
+    )
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    book = models.ForeignKey(to=Book, on_delete=models.CASCADE, related_name='reviews')
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    rate = models.IntegerField(choices=RATE_CHOICES)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
