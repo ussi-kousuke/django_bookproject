b58323c08cafde4decd9b6c43122dec484547e0b
root <root@DESKTOP-HLMRNO7.localdomain>
2022-09-20 11:19:27 +0900

    deploy-heroku

--- news/views.py
+++ news/views.py
@@ -281,8 +281,10 @@ class DetailBooKView(object):
         for search_result in search_response:
 
             youtube_title = search_result['snippet']['title']
-
-            youtube_video_id = search_result['id']['videoId']
+            try:
+                youtube_video_id = search_result['id']['videoId']
+            except KeyError:
+                pass
             youtube_video_url = f'https://www.youtube.com/watch?v={youtube_video_id}'
 
             youtube_video_url_dict[youtube_title] = youtube_video_url
