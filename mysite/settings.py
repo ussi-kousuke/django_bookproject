c5f901e059068f84a4de79b1fd4ccdb2f2bfffe5
root <root@DESKTOP-HLMRNO7.localdomain>
2022-10-07 00:50:33 +0900

    deploy-heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY', str)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['django-bookproject-portfolio.herokuapp.com']
 # django-bookproject-portfolio.herokuapp.com
 # '127.0.0.1' 
 # Application definition
