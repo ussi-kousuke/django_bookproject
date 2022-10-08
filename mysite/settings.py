90e6ba21421e3b1a42f065f56f33d7fc7adfb2b2
root <root@DESKTOP-HLMRNO7.localdomain>
2022-10-09 00:40:57 +0900

    deploy-heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY', str)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 # django-bookproject-portfolio.herokuapp.com
